d751fa0fab31ef7a83a6663facbc294a24a65b96
Castell11383 <rcastellanos11383@gmail.com>
2024-07-22 22:37:32 -0600

    Castellanos_Rivera_crudjs

---
+++ models/Cliente.php
@@ -0,0 +1,68 @@
+<?php
+require_once 'Conexion.php';
+
+class Cliente extends Conexion
+{
+    public $cliente_id;
+    public $cliente_nombre;
+    public $cliente_apellido;
+    public $cliente_genero;
+    public $cliente_situacion;
+
+
+    public function __construct($args = [])
+    {
+        $this->cliente_id = $args['cliente_id'] ?? null;
+        $this->cliente_nombre = $args['cliente_nombre'] ?? '';
+        $this->cliente_apellido = $args['cliente_apellido'] ?? '';
+        $this->cliente_genero = $args['cliente_genero'] ?? '';
+        $this->cliente_situacion = $args['cliente_situacion'] ?? '';
+    }
+
+    public function guardar()
+    {
+        $sql = "INSERT INTO clientes(cliente_nombre, cliente_apellido, cliente_genero) values('$this->cliente_nombre','$this->cliente_apellido','$this->cliente_genero')";
+        $resultado = self::ejecutar($sql);
+        return $resultado;
+    }
+
+    public function buscar()
+    {
+        $sql = "SELECT * from clientes where cliente_situacion = 1 ";
+
+        if ($this->cliente_nombre != '') {
+            $sql .= " and cliente_nombre like '%$this->cliente_nombre%' ";
+        }
+
+        if ($this->cliente_apellido != '') {
+            $sql .= " and cliente_apellido = $this->cliente_apellido ";
+        }
+
+        if ($this->cliente_genero != '') {
+            $sql .= " and cliente_genero = $this->cliente_genero ";
+        }
+
+        if ($this->cliente_id != null) {
+            $sql .= " and cliente_id = $this->cliente_id ";
+        }
+
+        $resultado = self::servir($sql);
+        return $resultado;
+    }
+
+    public function modificar()
+    {
+        $sql = "UPDATE clientes SET cliente_nombre = '$this->cliente_nombre', cliente_apellido = $this->cliente_apellido where cliente_id = $this->cliente_id";
+
+        $resultado = self::ejecutar($sql);
+        return $resultado;
+    }
+
+    public function eliminar()
+    {
+        $sql = "UPDATE clientes SET cliente_situacion = 0 where cliente_id = $this->cliente_id";
+
+        $resultado = self::ejecutar($sql);
+        return $resultado;
+    }
+}
---
+++ src/js/cliente/index.js
@@ -0,0 +1,151 @@
+const btnGuardar = document.getElementById('btnGuardar')
+const btnModificar = document.getElementById('btnModificar')
+const btnBuscar = document.getElementById('btnBuscar')
+const btnCancelar = document.getElementById('btnCancelar')
+const btnLimpiar = document.getElementById('btnLimpiar')
+const tablaCliente = document.getElementById('tablaCliente')
+const formulario = document.querySelector('form')
+
+btnModificar.parentElement.style.display = 'none'
+btnCancelar.parentElement.style.display = 'none'
+
+const getCliente = async () => {
+    const nombre = formulario.cliente_nombre.value
+    const apellido = formulario.cliente_apellido.value
+    const genero = formulario.cliente_genero.value
+    const url = `/castellanos_rivera_crudjs/controllers/cliente/index.php?cliente_nombre=${nombre}&cliente_apellido=${apellido}&cliente_genero=${genero}`
+    const config = {
+        method: 'GET'
+    }
+
+    try {
+        const respuesta = await fetch(url, config);
+        const data = await respuesta.json();
+
+        tablaClientes.tBodies[0].innerHTML = ''
+        const fragment = document.createDocumentFragment()
+        let contador = 1;
+        console.log(data);
+        if (respuesta.status == 200) {
+            Swal.mixin({
+                toast: true,
+                position: "top-end",
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                icon: "success",
+                title: 'Clientes encontrados',
+                didOpen: (toast) => {
+                    toast.onmouseenter = Swal.stopTimer;
+                    toast.onmouseleave = Swal.resumeTimer;
+                }
+            }).fire();
+
+            if (data.length > 0) {
+                data.forEach(cliente => {
+                    const tr = document.createElement('tr')
+                    const celda1 = document.createElement('td')
+                    const celda2 = document.createElement('td')
+                    const celda3 = document.createElement('td')
+                    const celda4 = document.createElement('td')
+                    const celda5 = document.createElement('td')
+                    const buttonModificar = document.createElement('button')
+                    const buttonEliminar = document.createElement('button')
+
+                    celda1.innerText = contador;
+                    celda2.innerText = cliente.cliente_nombre;
+                    celda3.innerText = cliente.cliente_apellido;
+                    celda3.innerText = cliente.cliente_genero;
+
+
+                    buttonModificar.textContent = 'Modificar'
+                    buttonModificar.classList.add('btn', 'btn-warning', 'w-100')
+
+                    buttonEliminar.textContent = 'Eliminar'
+                    buttonEliminar.classList.add('btn', 'btn-danger', 'w-100')
+
+                    celda4.appendChild(buttonModificar)
+                    celda5.appendChild(buttonEliminar)
+
+                    tr.appendChild(celda1)
+                    tr.appendChild(celda2)
+                    tr.appendChild(celda3)
+                    tr.appendChild(celda4)
+                    tr.appendChild(celda5)
+                    fragment.appendChild(tr);
+
+                    contador++
+                });
+
+            } else {
+                const tr = document.createElement('tr')
+                const td = document.createElement('td')
+                td.innerText = 'No hay clientes disponibles'
+                td.colSpan = 5;
+
+                tr.appendChild(td)
+                fragment.appendChild(tr)
+            }
+        } else {
+            console.log('hola');
+        }
+
+        tablaClientes.tBodies[0].appendChild(fragment)
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// getClientes();
+
+
+const guardarCliente = async (e) => {
+    e.preventDefault();
+    btnGuardar.disabled = true;
+
+    const url = '/castellanos_rivera_crudjs/controllers/cliente/index.php'
+    const formData = new FormData(formulario)
+    formData.append('tipo', 1)
+    formData.delete('cliente_id')
+    const config = {
+        method: 'POST',
+        body: formData
+    }
+
+    try {
+        const respuesta = await fetch(url, config);
+        const data = await respuesta.json();
+        const { mensaje, codigo, detalle } = data
+        console.log(data);
+        Swal.mixin({
+            toast: true,
+            position: "top-end",
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            icon: "success",
+            title: mensaje,
+            didOpen: (toast) => {
+                toast.onmouseenter = Swal.stopTimer;
+                toast.onmouseleave = Swal.resumeTimer;
+            }
+        }).fire();
+        // alert(mensaje)
+        // console.log(data);
+        if (codigo == 1 && respuesta.status == 200) {
+            // getProductos();
+            formulario.reset();
+        } else {
+            console.log(detalle);
+        }
+
+    } catch (error) {
+        console.log(error);
+    }
+    btnGuardar.disabled = false;
+}
+
+
+
+formulario.addEventListener('submit', guardarProducto)
+btnBuscar.addEventListener('click', getProductos)
